2daa89e2929ef870059d75220b407382688c3443
Daniel <115842725+DanielDeveloperBR@users.noreply.github.com>
2023-08-16 20:02:49 -0300

    rota noticias

---
+++ controllers/rootController.js
@@ -0,0 +1,9 @@
+import path from "path"
+function rootController(app) {
+    app.get("/", exibir)
+    function exibir(req,res){
+        res.sendFile(path.join(path.resolve(), "./controllers/documentacao.html"))
+    }
+}
+
+export default rootController
